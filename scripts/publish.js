eea8bf9d339305df7d1ff74eab93eb5f0b221649
Douglas Nassif Roma Junior <douglasjunior@users.noreply.github.com>
2023-10-21 13:41:01 -0300

    Update publish.js

--- scripts/publish.js
+++ scripts/publish.js
@@ -3,12 +3,7 @@ import fs from 'fs';
 import child_process from 'child_process';
 import { fileURLToPath } from 'url';
 
-const loadJSON = (path) => JSON.parse(fs.readFileSync(new URL(path, import.meta.url)));
-
-const packageJson = loadJSON('../package.json');
-
-packageJson.main = 'dist';
-packageJson.types = 'dist';
+const loadJSON = (filePath) => JSON.parse(fs.readFileSync(filePath));
 
 const __filename = fileURLToPath(import.meta.url);
 
@@ -16,18 +11,23 @@ const __dirname = path.dirname(__filename);
 
 const packageJsonPath = path.resolve(__dirname, '..', 'package.json');
 
-const content = JSON.stringify(packageJson, null, 2) + '\n';
-
-child_process.execSync('npm run build');
-
-fs.writeFileSync(packageJsonPath, content, {
-  encoding: 'utf-8',
-});
-
 try {
+  const packageJson = loadJSON(packageJsonPath);
+  
+  packageJson.main = 'dist';
+  packageJson.types = 'dist';
+  
+  const content = JSON.stringify(packageJson, null, 2) + '\n';
+  
+  child_process.execSync('npm run build');
+  
+  fs.writeFileSync(packageJsonPath, content, {
+    encoding: 'utf-8',
+  });
+
   child_process.execSync('npm publish');
 } catch (err) {
   console.error(err);
+} finally {
+  child_process.execSync('git checkout -- "' + packageJsonPath + '"');
 }
-
-child_process.execSync('git checkout -- "' + packageJsonPath + '"');
